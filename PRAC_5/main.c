60c7521898d3d72e87ba33f9c6bffea02266739a
ukarsh2004git <utkarshshahare2004@gmail.com>
2023-10-28 11:31:13 +0530

    prac5

--- PRAC_5/main.c
+++ PRAC_5/main.c
@@ -6,22 +6,50 @@ struct Dnode{
     struct Dnode *prev,*next;
 };
 
+//GLOBAL
 struct Dnode *head;
 
+//creating  head
 void create(int ele){
     if(head==NULL){
         head=(struct Dnode *)malloc(sizeof(struct Dnode));
         head->data=ele;
+        head->prev=NULL;
+        head->next=NULL;
+        printf("Head is Created\n");
     }
-
-    else
-    {
+    else{
         printf("Already Created \n");
-    }
-    
+    }  
 }
 
+// Adding element in linked list
+
+void add_begin(int ele){
+    struct Dnode *temp;
+    temp->data=ele;
+    temp->next=head;
+    head->prev=temp;
+    head=temp;
+    printf("Element Added at begin\n");
+}
+
+void add_end(int ele){
+    struct Dnode *temp,*prev;
+    temp=head;
+    while(temp->next!=NULL){
+        prev=temp;
+        temp=temp->next;
+    }
+    temp->data=ele;
+    temp->prev=prev;
+    temp->next=NULL;
+    printf("Element Added at end\n");
+}
 int main(){
-    
+    create(10);
+    // add_begin(5);
+    // add_begin(5);
+    add_end(5);
     return 0;
 }
