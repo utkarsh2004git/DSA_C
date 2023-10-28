4daf90d6a766ed8c488c63c4fb6b5ff2c6afc380
ukarsh2004git <utkarshshahare2004@gmail.com>
2023-10-28 11:12:27 +0530

    code for prac5

--- PRAC_5/main.c
+++ PRAC_5/main.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Dnode{
+    int data;
+    struct Dnode *prev,*next;
+};
+
+struct Dnode *head;
+
+void create(int ele){
+    if(head==NULL){
+        head=(struct Dnode *)malloc(sizeof(struct Dnode));
+        head->data=ele;
+    }
+
+    else
+    {
+        printf("Already Created \n");
+    }
+    
+}
+
 int main(){
     
     return 0;
